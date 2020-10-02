7c0fe28f9d812e33b73e279f3d0adf89bf57e5e0
Matthew Fitzpatrick <realmattyfitzz@Matthews-MacBook-Pro.local>
2020-10-02 13:49:59 -0700

    functionality added, offline mode unsure

---
+++ public/cached.js
@@ -0,0 +1,72 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+
+  self.addEventListener("fetch", (evt) => {
+    // cache successful "get" requests to the API
+    if (evt.request.url.includes("/api") && evt.request.method === "GET") {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then((cache) => {
+            return fetch(evt.request)
+              .then((response) => {
+                // if the response is good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request, response.clone());
+                }
+                return response;
+              })
+              .catch(() => {
+                // if online request fails, try to get it from the cache.
+                return cache.match(evt.request);
+              });
+          })
+          .catch((err) => console.log(err))
+      );
+      return;
+    }
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      })
+    );
+  });
---
+++ public/db.js
@@ -0,0 +1,57 @@
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+   // object store called "pending" and set autoIncrement to true
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  console.log("Error: " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
+
+function checkDatabase() {
+  // opens a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
