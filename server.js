a2d93aebb30e6fca25402503306e8face1ff0d27
Matthew Fitzpatrick <realmattyfitzz@Matthews-MacBook-Pro.local>
2020-10-05 11:19:05 -0700

    commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgeter", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Budgeter", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
