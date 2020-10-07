a94383876c6235de3428736e0fd95c1604e0e58d
Matthew Fitzpatrick <realmattyfitzz@Matthews-MacBook-Pro.local>
2020-10-07 13:25:43 -0700

    deploy

--- public/db.js
+++ public/db.js
@@ -35,7 +35,7 @@ function checkDatabase() {
 
   getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
+      fetch("/api/transaction", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
